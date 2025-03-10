0819e78e49d300bb6b653e90b7fb0d10be12d497
Slddev <sldemonatnoiv@gmail.com>
2025-03-10 22:38:58 +0800

    feat: add supabase layer

---
+++ utils/middleware.ts
@@ -0,0 +1,84 @@
+import { getUserById } from "@/queries/getUserById";
+import { createServerClient } from "@supabase/ssr";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+
+  // IMPORTANT: Avoid writing any logic between createServerClient and
+  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+  // issues with users being randomly logged out.
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/auth/") &&
+    !request.nextUrl.pathname.startsWith("/api/") &&
+    request.nextUrl.pathname != "/"
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  if (user) {
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+      return NextResponse.redirect(new URL("/dashboard/", request.url));
+    }
+
+    if (request.nextUrl.pathname.startsWith("/dashboard/admin/")) {
+      const { data, error } = await getUserById(supabase, user!.id);
+
+      if (error) throw error;
+      if (data) {
+        const isAdmin =
+          data.map((e) => e.role).toString() == "admin" ? true : false;
+        if (!isAdmin) {
+          return NextResponse.redirect(new URL("/dashboard/", request.url));
+        }
+      }
+    }
+  }
+
+  // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
+  // creating a new response object with NextResponse.next() make sure to:
+  // 1. Pass the request in it, like so:
+  //    const myNewResponse = NextResponse.next({ request })
+  // 2. Copy over the cookies, like so:
+  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+  // 3. Change the myNewResponse object to fit your needs, but avoid changing
+  //    the cookies!
+  // 4. Finally:
+  //    return myNewResponse
+  // If this is not done, you may be causing the browser and server to go out
+  // of sync and terminate the user's session prematurely!
+
+  return supabaseResponse;
+}
